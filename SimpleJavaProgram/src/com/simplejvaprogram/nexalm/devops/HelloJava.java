df32bd503d2a5290bfdd22beb8195d941096c330
ChaitaliTungar <ChaitaliTungar@gmail.com>
2023-02-23 14:14:18 +0530

    Commited some changes

--- SimpleJavaProgram/src/com/simplejvaprogram/nexalm/devops/HelloJava.java
+++ SimpleJavaProgram/src/com/simplejvaprogram/nexalm/devops/HelloJava.java
@@ -5,9 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java");
-		System.out.println("Commit Changes");
-		System.out.println("GitHUbTest");
-		System.out.println("chnage");
+	
 	}
 
 }
